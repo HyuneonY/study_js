4e2c962bf8f9e752936f47f6ae52e0789f33315c
HeY1996 <kkx3695@gmail.com>
2024-04-24 01:44:07 +0900

    week_2

---
+++ study_js/week_2/01.js
@@ -0,0 +1,27 @@
+// ES6
+
+// 2015년도 이전 => var
+// (1) es6 => let(변수), cosnt(상수)
+
+// (2) arrow function
+function add() {
+
+}
+
+var add = function () {
+
+}
+
+var add = (x) => {
+    return i;
+}
+
+var add = (x) => i;
+
+// (3) 삼향 연산자
+// condition ? true인경우 : false인경우
+
+console.log(true ? '참' : '거짓');
+console.log(false ? '참' : '거짓');
+console.log(1 === 1 ? '참' : '거짓');
+console.log(1 !== 1 ? '참' : '거짓');
